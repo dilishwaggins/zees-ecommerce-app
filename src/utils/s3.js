79e88404b9b45d0de1ea1140717064a9f8c29444
Zawadi <82971560+dilishwaggins@users.noreply.github.com>
2025-10-12 21:20:55 +0300

    update

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -11,7 +11,7 @@ const s3 = new AWS.S3({
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.S3_BUCKET,
+    bucket: process.env.AWS_BUCKET_NAME,
     acl: "public-read",
     key: function (req, file, cb) {
       cb(null, `products/${Date.now()}_${file.originalname}`);
