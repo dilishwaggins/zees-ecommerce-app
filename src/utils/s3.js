761ff0366ccb519341a514d90a28621c0ec42999
Zawadi <82971560+dilishwaggins@users.noreply.github.com>
2025-10-12 21:15:38 +0300

    update

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -8,6 +8,15 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
 });
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.S3_BUCKET,
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(null, `products/${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
 
 module.exports = upload;
